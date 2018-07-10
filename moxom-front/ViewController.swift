b1133b8ef38556d7888130f14dd289916ce9d96b
Santosh <santosh.guruju@icloud.com>
2018-07-11 00:18:24 +0800

    Initial Commit

---
+++ moxom-front/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  moxom-front
+//
+//  Created by apple on 7/11/18.
+//  Copyright © 2018 woorkstreak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
